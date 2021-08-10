663dc260523a204ed4d4ee579807ac2b5100c499
akash <akashbanaulikar05@gmail.com>
2021-08-10 14:37:43 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const config = require("./config/key");
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+const connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
-app.use(cors())
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // routes
-app.use('/api/users', require('./routes/user'));
+app.use("/api/users", require("./routes/user"));
 
-app.use('/api/pdfData', require('./routes/pdfData'));
+app.use("/api/pdfData", require("./routes/pdfData"));
 
-const port = config.PORT || 5000
+const port = config.port;
 
 app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
+  console.log(`Server Listening on ${port}`);
 });
